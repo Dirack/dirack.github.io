9dad558768124c5887844676e3925d9eede3d588
Dirack <rodolfo_profissional@hotmail.com>
2022-04-16 21:29:49 -0300

    Finaliza o footer da página

--- includes/rodape.php
+++ includes/rodape.php
@@ -3,72 +3,88 @@
 ?>
 
 <footer>
-	<div id="img-rodape"></div>
+	<div class="footer-container">
+		<div id="links-rodape">
+			<div class="links-rodape__content">
+				<div class="links-rodape__title">
+					<h2>Suporte</h2>
+				</div>
+				<div class="links-rodape__list">
+					<ul>
+						<li>
+							<a href="https://github.com/Geofisicando">Geofisicando</a>
+						</li>
+						<li>
+							<a href="https://github.com/gpgeof" title="Grupo de programação aplicada à Geofísica (GPGEOF)">GPGEOF</a>
+						</li>
+						<li>Perguntas frequentes</li>
+						<li>
+							<a target="_blank" href="https://www.gnu.org/licenses/quick-guide-gplv3.pt-br.html">Licença de uso (GPL 3)</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 
-	<div id="links-rodape">
-		<div id="links-rodape-ajuda">
-			<h2>Suporte</h2>
-			<ul>
-				<li>
-					<a href="https://github.com/Geofisicando">Geofisicando</a>
-				</li>
-				<li>
-					<a href="https://github.com/gpgeof" title="Grupo de programação aplicada à Geofísica (GPGEOF)">GPGEOF</a>
-				</li>
-				<li>Perguntas frequentes</li>
-				<li>
-					<a target="_blank" href="https://www.gnu.org/licenses/quick-guide-gplv3.pt-br.html">Licença de uso (GPL 3)</a>
-				</li>
-			</ul>
-	</div>
-	<div id="links-rodape-sobre">
-		<h2>Sobre</h2>
-			<ul>
-				<li>
-					<a href="about/contato.php#canvas">Quem somos</a>
-				</li>
-				<li>
-					<a href="about/biografia.html#canvas">O Autor</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://apoia.se/geofisicando">Seja membro (APOIASE)</a>
-				</li>
-			</ul>
-	</div>
-	<div id="links-rodape-redes-sociais">
-		<h2>Redes Sociais</h2>
+			<div class="links-rodape__content">
+				<div class="links-rodape__title">
+					<h2>Sobre</h2>
+				</div>
+				<div class="links-rodape__list">
+					<ul>
+						<li>
+							<a href="about/contato.php#canvas">Quem somos</a>
+						</li>
+						<li>
+							<a href="about/biografia.html#canvas">O Autor</a>
+						</li>
+						<li>
+							<a target="_blank" href="https://apoia.se/geofisicando">Seja membro (APOIASE)</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 
-			<ul id="img-redes-sociais">
-				<li>
-					<a target="_blank" href="https://www.youtube.com/c/Geofisicando" title="Geofisicando no Youtube">
-						<img src="<?php echo $ROOT_PATH;?>images/youtube.png">
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://www.linkedin.com/in/rodolfo-andr%C3%A9-cardoso-neves-49a0778b/" title="Linkedin">
-						<img src="<?php echo $ROOT_PATH;?>images/linkedin.png">
-					</a>
-				</li>
-				<li>
-					<a taget="_blank" href="https://twitter.com/RodolfoDirack" title="Twitter">
-						<img src="<?php echo $ROOT_PATH;?>images/twitter.png">
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://www.instagram.com/rodolfodirack/?hl=pt-br" title="Instagram">
-						<img src="<?php echo $ROOT_PATH;?>images/instagram.png">
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://www.facebook.com/geofisicando" title="Facebook">
-						<img src="<?php echo $ROOT_PATH;?>images/face.png">
-					</a>
-				</li>
-			</ul>
+			<div class="links-rodape__content">
+				<div class="links-rodape__title">
+					<h2>Redes Sociais</h2>
+				</div>
 
+				<div class="links-rodape__list">
+					<ul class="links-rodape__list-imgs">
+						<li>
+							<a target="_blank" href="https://www.youtube.com/c/Geofisicando" title="Geofisicando no Youtube">
+								<img src="<?php echo $ROOT_PATH;?>images/youtube.png">
+							</a>
+						</li>
+						<li>
+							<a target="_blank" href="https://www.linkedin.com/in/rodolfo-andr%C3%A9-cardoso-neves-49a0778b/" title="Linkedin">
+								<img src="<?php echo $ROOT_PATH;?>images/linkedin.png">
+							</a>
+						</li>
+						<li>
+							<a taget="_blank" href="https://twitter.com/RodolfoDirack" title="Twitter">
+								<img src="<?php echo $ROOT_PATH;?>images/twitter.png">
+							</a>
+						</li>
+						<li>
+							<a target="_blank" href="https://www.instagram.com/rodolfodirack/?hl=pt-br" title="Instagram">
+								<img src="<?php echo $ROOT_PATH;?>images/instagram.png">
+							</a>
+						</li>
+						<li>
+							<a target="_blank" href="https://www.facebook.com/geofisicando" title="Facebook">
+								<img src="<?php echo $ROOT_PATH;?>images/face.png">
+							</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+		<div class="direitos">
+			&copy; 2018, Rodolfo Dirack. Todos os direitos reservados.
+		</div>
 	</div>
-</div>
-&copy; 2018, Rodolfo Dirack. Todos os direitos reservados.
-</footer
+</footer>
 
 
